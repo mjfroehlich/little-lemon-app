df7a3858e0d952d11e57936fb688f5994ff94ac4
Martin Froehlich <4023770+mjfroehlich@users.noreply.github.com>
2024-02-26 14:23:32 +0200

    Wireframe, basic app setup

---
+++ LittleLemonApp/LittleLemonApp/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonAppApp.swift
+//  LittleLemonApp
+//
+//  Created by Martin Froehlich on 26.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
